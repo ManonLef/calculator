98e925ab28035341cdfbb504a30ea123382db990
Manon <manonlefkos@gmail.com>
2022-07-30 22:53:50 +0200

    add multiply and divide function

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ function substract(num1, num2) {
     return num1 - num2;
 };
 
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+function divide(num1, num2) {
+    return num1 / num2;
+};
