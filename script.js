a6e42b2d7fedd604a704f417226cd7ac2785a970
Manon <manonlefkos@gmail.com>
2022-07-30 23:07:51 +0200

    add operate function

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 };
+
+/* Create a new function operate that takes an operator and 
+2 numbers and then calls one of the above functions on the numbers. 
+*/
+
+function operate(num1, operator, num2) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return substract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    } else {
+        return "ERROR";
+    };
+};
