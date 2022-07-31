c2a47ed0610c82b6add6ce242e432fb1dd4c8f6c
Manon <manonlefkos@gmail.com>
2022-07-31 06:45:50 +0200

    simplify operate function

--- script.js
+++ script.js
@@ -26,15 +26,5 @@ function divide(num1, num2) {
 */
 
 function operate(num1, operator, num2) {
-    if (operator === "+") {
-        return add(num1, num2);
-    } else if (operator === "-") {
-        return substract(num1, num2);
-    } else if (operator === "*") {
-        return multiply(num1, num2);
-    } else if (operator === "/") {
-        return divide(num1, num2);
-    } else {
-        return "ERROR";
-    };
+    return operator(num1, num2);
 };
