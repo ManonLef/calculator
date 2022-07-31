38882a0a25042302526c0544cb3793832f660482
Manon <manonlefkos@gmail.com>
2022-07-31 13:43:21 +0200

    add operatorBtn class and apply to operators

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-/* add several functions for calculator:
-- add
-- substract
-- multiply
-- divide 
-*/
+const display = document.querySelector('#inputDisplay');
+const buttons = document.querySelectorAll('.btn');
+let displayValue = "";
 
 function add(num1, num2) {
     return num1 + num2;
@@ -21,24 +18,10 @@ function divide(num1, num2) {
     return num1 / num2;
 };
 
-/* Create a new function operate that takes an operator and 
-2 numbers and then calls one of the above functions on the numbers. 
-*/
-
+//function that calls on above functions depending on operator
 function operate(num1, operator, num2) {
     return operator(num1, num2);
 };
-
-/* Create the functions that populate the display when you click 
-the number buttons. You should be storing the ‘display value’ in a 
-variable somewhere for use in the next step.
-*/
-
-let displayValue = "";
-
-const display = document.querySelector('#inputDisplay');
-
-const buttons = document.querySelectorAll('.btn');
 
 // number buttons to fill display when clicked
 buttons.forEach((btn) => {
